26ce6468b0325b434ae26088bc2c8aa04f82a24c
DmytroSovaV <ikrosua@gmail.com>
2025-04-25 13:23:26 +0300

    fixed swiper

--- swiper.js
+++ swiper.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
-  loop: false,
+  // loop: false,
 
   // If we need pagination
   // pagination: {
@@ -18,24 +18,24 @@ const swiper = new Swiper('.swiper', {
   // scrollbar: {
   //   el: '.swiper-scrollbar',
   // },
-  //  slidesPerView: 1,
-  // spaceBetween: 10,
-  // // Responsive breakpoints
-  // breakpoints: {
-  //   // when window width is >= 320px
-  //   320: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20
-  //   },
-  //   // when window width is >= 480px
-  //   480: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30
-  //   },
-  //   // when window width is >= 640px
-  //   640: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 40
-  //   }
-  // }
+   slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1040: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
 });
